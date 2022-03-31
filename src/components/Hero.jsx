dffa0bf5c952da898b27e6b914f13e6c32b4b9c4
Emassey123 <94918775+Emassey123@users.noreply.github.com>
2022-03-31 10:32:11 -0400

    fixed another broken link in the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <section className="hero-text">
         <h1 className="hero-title">Passion for a team?</h1>
         <p className="hero-subtitle">We can help share it!</p>
-        <a className="heroButton heroButtonShop" href="/products">
+        <a className="heroButton heroButtonShop" href="/product">
           Shop
         </a>
       </section>
